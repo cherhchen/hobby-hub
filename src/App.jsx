9a16c1999a4512463fb1f5dfda691cb69893292c
cherhchen <54284307+cherhchen@users.noreply.github.com>
2025-04-21 23:05:35 -0700

    fixed supabase env

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
 import Post from './components/Post'
+import { supabase } from './client'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const URL = import.meta.env.VITE_SUPABASE_URL;
-const anonKey = import.meta.env.VITE_SUPABASE_URL;
+const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, anonKey)
